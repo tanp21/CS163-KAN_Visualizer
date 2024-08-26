6f38e526fb52e98f246f765a02c3c9e6a6ed96a4
Tai Pham Nguyen Anh <anhtaipham0521@gmail.com>
2024-08-26 19:38:08 +0700

    add AutoTexture class to Engine.h

--- Engine.h
+++ Engine.h
@@ -1,4 +1,52 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include "raylib.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+class AutoTexture {
+private:
+    Texture2D texture;
+public:
+    AutoTexture(const std::string& filePath) {
+        texture = LoadTexture(filePath.c_str());
+        if (texture.id == 0) {
+            std::cerr << "Failed to load texture: " << filePath << std::endl;
+        } else {
+            std::cout << "Texture loaded: " << filePath << std::endl;
+        }
+    }
+
+    ~AutoTexture() {
+        if (texture.id != 0) {
+            UnloadTexture(texture);
+            std::cout << "Texture unloaded." << std::endl;
+        }
+    }
+
+    Texture2D getTexture() const {
+        return texture;
+    }
+
+    AutoTexture(const AutoTexture&) = delete;
+    AutoTexture& operator=(const AutoTexture&) = delete;
+
+    AutoTexture(AutoTexture&& other) noexcept : texture(other.texture) {
+        other.texture.id = 0;
+    }
+
+    AutoTexture& operator=(AutoTexture&& other) noexcept {
+        if (this != &other) {
+            if (texture.id != 0) {
+                UnloadTexture(texture);
+            }
+            texture = other.texture;
+            other.texture.id = 0;
+        }
+        return *this;
+    }
+};
+
 #endif
--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
+// Frontend
 #include "raylib.h"
+#include "Engine.h"
+
+// Backend
+#include "./KAN/KAN.h"
 
 const int screenWidth = 1920;
 const int screenHeight = 1080;
@@ -6,16 +11,18 @@ const int screenHeight = 1080;
 int main() {
     InitWindow(screenWidth, screenHeight, "KAN Visualization");
 
-    Texture2D background = LoadTexture("resources/StartPage/background.png");
+    // Texture2D background = LoadTexture("resources/StartPage/background.png");
 
     SetTargetFPS(60);
     ToggleFullscreen();
+
+    AutoTexture tex("resources/StartPage/background.png");
 
     while(!WindowShouldClose()) {
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-        DrawTexture(background, 95, 7, WHITE);
+        DrawTexture(tex.getTexture(), 95, 7, WHITE);
 
         EndDrawing();
     }
