de3eff2744c1ee398acc51d75766a6850732dd06
Tai Pham Nguyen Anh <anhtaipham0521@gmail.com>
2024-08-26 10:24:06 +0700

    add initial content to spline.h

---
+++ KAN/spline.h
@@ -0,0 +1,39 @@
+#ifndef SPLINE_H
+#define SPLINE_H
+
+#include <bits/stdc++.h>
+#include "utils.h"
+
+namespace KANN {
+    class B_Spline {
+        private:
+
+        std::vector<float> bases;
+        std::vector<float> coef;
+        std::vector<float> coef_grad;
+
+        float db_dx;
+        float last;
+
+        int spline_order;
+        int num_points;
+        int num_bases;
+
+        public:
+
+        B_Spline(int spline_order = 3, int num_points = grid.size());
+
+        void cox_de_boor(float x);
+
+        float derivative();
+
+        float eval(float x);
+
+        float get_last() const;
+
+        void backward(float last_d);
+
+        void updateAll(float lr, int epoch_size);
+    };
+};
+#endif
