b9fb977a216d6a361c5d127a16b0d56d9e2e878b
Tai Pham Nguyen Anh <anhtaipham0521@gmail.com>
2024-08-28 11:01:27 +0700

    add ButtonTexture class into EngineSupport.h

--- EngineSupport.h
+++ EngineSupport.h
@@ -28,11 +28,11 @@ public:
         }
     }
 
-    void draw() const {
+    virtual void draw() const {
         DrawTexture(texture, x, y, WHITE);
     }
 
-    void setPosition(int newX, int newY) {
+    virtual void setPosition(float newX, float newY) {
         x = newX;
         y = newY;
     }
@@ -61,6 +61,76 @@ public:
         }
         return *this;
     }
+};
+
+class ButtonTexture : public AutoTexture {
+private:
+    enum ButtonState {
+        NORMAL,
+        HOVER,
+        PRESS,
+    };
+
+    ButtonState state;
+
+    Rectangle bounds;    // Rectangle to hold the position and size of the button
+    float scaleFactor;   // Factor to scale the button when pressed
+    Color normalColor;   // Normal state color (white)
+    Color hoverColor;    // Hover state color (darker)
+    Color pressedColor;  // Pressed state color (even darker)
+    Color currentColor;  // Current state color
+
+public:
+    ButtonTexture(const std::string& filePath, float initialX = 0, float initialY = 0) 
+        : AutoTexture(filePath, initialX, initialY), scaleFactor(0.9f), normalColor(WHITE), hoverColor(GRAY), pressedColor(DARKGRAY), state(NORMAL) {
+        
+        if (getTexture().id != 0) {
+            bounds = { initialX, initialY, static_cast<float>(getTexture().width), static_cast<float>(getTexture().height) };
+            currentColor = normalColor;
+        }
+    }
+
+    bool action() {
+        bool is_action = false;
+
+        if (CheckCollisionPointRec(GetMousePosition(), bounds)) {
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                state = PRESS;
+                currentColor = pressedColor;
+            }
+            else {
+                state = HOVER;
+                currentColor = hoverColor;
+            }
+
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) is_action = true;
+        }
+        else {
+            state = NORMAL;
+            currentColor = normalColor;
+        }
+
+        return is_action;
+    }
+
+    void draw() const override {
+        if (state == PRESS) {
+            DrawTextureEx(getTexture(), { bounds.x + bounds.width * (1 - scaleFactor) / 2, 
+                                          bounds.y + bounds.height * (1 - scaleFactor) / 2 }, 
+                          0.0f, scaleFactor, currentColor);
+        } else {
+            DrawTexture(getTexture(), bounds.x, bounds.y, currentColor);
+        }
+    }
+
+    void setPosition(float newX, float newY) override {
+        AutoTexture::setPosition(newX, newY);
+        bounds.x = newX;
+        bounds.y = newY;
+    }
+
+    float getX() const { return bounds.x; }
+    float getY() const { return bounds.y; }
 };
 
 #endif
